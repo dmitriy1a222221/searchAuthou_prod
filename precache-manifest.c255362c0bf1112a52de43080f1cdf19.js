0fdc6225d78cd632718f0fcdc92caf588c9b3ebf
dmitriy <dmitriy1a22222@gmail.com>
2019-02-08 21:14:57 +0200

    first commit

---
+++ precache-manifest.c255362c0bf1112a52de43080f1cdf19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a502666c939561b6a82",
+    "url": "/static/js/main.1a502666.chunk.js"
+  },
+  {
+    "revision": "c19f9d83ab069f518236",
+    "url": "/static/js/1.c19f9d83.chunk.js"
+  },
+  {
+    "revision": "1a502666c939561b6a82",
+    "url": "/static/css/main.c9a9d7f1.chunk.css"
+  },
+  {
+    "revision": "55eeab56beb742f96fd43151c0477cce",
+    "url": "/index.html"
+  }
+];
